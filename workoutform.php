82be9b6c52317240674217598551fd49055ccc19
sdthalagahawatte <136792556+sdthalagahawatte@users.noreply.github.com>
2023-07-15 23:06:34 +0530

    Add files via upload

---
+++ workoutform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
+	
+	$mailto = "damiruninduwara@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+	
+	mail($mailto, $subject, $txt, $headers);
+	header("Location: https://zeuzfit.com/Workouts.html/?mailsend");
+ }
